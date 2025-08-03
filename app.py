aff2d7f66635dc88c50769d37dfd83cbf51012fa
mayurtarate2 <mayur_tarate_aiml@moderncoe.edu.in>
2025-08-03 19:48:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1072,4 +1072,5 @@ Answer:"""
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
